ea4b9da9d864aedf1b4887003ec24498901f0f86
Natalia <natirgh@gmail.com>
2022-09-02 13:37:42 +0200

    installed jwt and made createToken function

--- src/utils/authenticate.ts
+++ src/utils/authenticate.ts
@@ -1,4 +1,6 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import JwtPayload from "../interfaces/JwtPayload";
 
 export const hashCreator = (password: string) => {
   const salt = 10;
@@ -9,3 +11,6 @@ export const hashCompare = (
   passwordEntered: string,
   passwordDBHashed: string
 ) => bcrypt.compare(passwordEntered, passwordDBHashed);
+
+export const createToken = (payload: JwtPayload) =>
+  jwt.sign(payload, process.env.SECRET);
