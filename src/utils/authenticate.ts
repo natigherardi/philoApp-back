1cde2b9192c8d01013195ab6a59d74ade297eb76
Natalia <natirgh@gmail.com>
2022-09-02 13:46:23 +0200

    verify token function made

--- src/utils/authenticate.ts
+++ src/utils/authenticate.ts
@@ -14,3 +14,6 @@ export const hashCompare = (
 
 export const createToken = (payload: JwtPayload) =>
   jwt.sign(payload, process.env.SECRET);
+
+export const verifyToken = (token: string) =>
+  jwt.verify(token, process.env.SECRET);
