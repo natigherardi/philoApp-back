86c1c99b03bffcec74f00e6d78ce2c6cfef324d9
Natalia <natirgh@gmail.com>
2022-09-02 10:22:15 +0200

    endpoint login in userRouter

--- src/server/routers/userRouter.ts
+++ src/server/routers/userRouter.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { validate } from "express-validation";
-import { registerUser } from "../controllers/usersController";
+import { loginUser, registerUser } from "../controllers/usersController";
 import userDataSchema from "../schemas/userDataSchema";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", validate(userDataSchema), registerUser);
+userRouter.post("/login", loginUser);
 
 export default userRouter;
