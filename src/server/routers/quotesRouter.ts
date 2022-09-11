a227d53c2fa33711b619ecbe04f728dfac5b7c5f
Natalia <natirgh@gmail.com>
2022-09-11 14:28:30 +0200

    installed multer and called it in quotes router with dest

--- src/server/routers/quotesRouter.ts
+++ src/server/routers/quotesRouter.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
 import {
   createQuote,
   deleteQuote,
@@ -9,9 +11,14 @@ import authUser from "../middlewares/authUser";
 
 const quotesRouter = express.Router();
 
+const uploader = multer({
+  dest: path.join("uploads", "img"),
+  limits: { fileSize: 800000 },
+});
+
 quotesRouter.get("/all-quotes", getAllQuotes);
 quotesRouter.get("/quotesByUser", authUser, getQuotesByUser);
 quotesRouter.delete("/quote", authUser, deleteQuote);
-quotesRouter.post("/quote", authUser, createQuote);
+quotesRouter.post("/quote", authUser, uploader.single("image"), createQuote);
 
 export default quotesRouter;
